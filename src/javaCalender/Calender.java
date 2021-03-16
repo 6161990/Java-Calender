e3f1cd6037ded5b655f24b28ef4c0b0d98cd6eab
6161990 <6161990src@gmail.com>
2021-03-16 10:03:08 +0900

    달에 맞는 일수 출력하기 array,%d, method

--- src/javaCalender/Calender.java
+++ src/javaCalender/Calender.java
@@ -1,17 +1,49 @@
 package javaCalender;
 
-public class Calender {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class Calender {
+	
+	private static int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+	}
+
+	public void printSampleCalender() {
 		System.out.println(" 일   월   화   수   목   금   토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-	
-	
 		
+	}
+	
+	public static void main(String[] args) {
+	
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		/*방법 1
+		System.out.println("달을 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month -1]);
+		*/
+		
+		//방법 2 (메소드를 만들고 난 후)
+		System.out.println("달을 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
+		int month = scanner.nextInt();
+		
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		
+		cal.printSampleCalender();
+		scanner.close();
 	}
 
 }

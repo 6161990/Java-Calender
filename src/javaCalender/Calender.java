529b96dd08a589cb71dccd7e9f89c5f6bf078af3
6161990 <6161990src@gmail.com>
2021-03-18 08:20:04 +0900

    autocally calender

--- src/javaCalender/Calender.java
+++ src/javaCalender/Calender.java
@@ -2,8 +2,8 @@ package javaCalender;
 
 public class Calender {
 	
-	private static int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	private static int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static int[] LEAP_MAX_DAYS = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 	public boolean isLeapYear(int year) {
 		if(year % 4 == 0 && (year % 100 !=0 || year % 400 == 0)){
@@ -14,47 +14,75 @@ public class Calender {
 	}
 	public int getMaxDaysOfMonth(int year, int month) {
 		if(isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month -1];
+			return LEAP_MAX_DAYS[month];
 		}else {
-		    return MAX_DAYS[month - 1];
+		    return MAX_DAYS[month];
 		}
 	}
-	public void printCalender(int year, int month,int weekday) {
-		System.out.printf("    <<%4d년 %3d월>>\n",year,month);
+	public void printCalender(int year, int month) {
+		System.out.printf("    <<%d년 %d월>>\n",year,month);
 		System.out.println(" 일   월   화   수   목   금   토");
 		System.out.println("---------------------");
+		
+		int weekday = getWeekday(year, month, 1);
 		
 		//print blank space
 		for(int i=0; i<weekday; i++) {
 			System.out.print("   ");
 		}
+		
 		int maxDay = getMaxDaysOfMonth(year, month);
 		
 		// print first line (confirm back days) 
 		int count = 7- weekday;
-		for(int i=1; i<=count; i++) {
+		for(int i = 1; i<=count; i++) {
 			System.out.printf("%3d", i);
 		}
 		System.out.println();
 
 		//print from second line to last
 		int delim = (count < 7)? count : 0;
-		System.out.println(count+"ㅣㅣㅣㅣ"); // 4||||
 		count++; 
 		
 		for(int i=count; i<=maxDay; i++) {
 			System.out.printf("%3d", i);  //해당 자리에 들어올 int가 총 세자리를 차지함.
 			if(i % 7 == delim) {
 				System.out.println();
-				System.out.println(delim);   // 4
-				System.out.println(count+"ㅣㅣㅣㅣ");  //5||||
-
 			}
-			
 	    }
 		System.out.println();
 		System.out.println();
 	
 	}
+	private int getWeekday(int year, int month, int day) {
+		//기준날짜의 요일을 알아낸다 1970년 1월 1일은 목요일
+		int syear = 1970;
+		final int STANDARD_WEEKDAY = 3;
+		
+		int count = 0;
+		
+		for(int i = syear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366: 365;
+			count += delta;
+		}
+		for(int i = 1; i < month; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count +=delta;
+		}
+		count += day; //1월 1일 더할 필요없음
+		
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
+		return weekday;
+	}
+	//simple test code here
+		public static void main(String[] args) {
+			Calender cal = new Calender();
+			System.out.println(cal.getWeekday(1970, 1, 1) == 3);
+			System.out.println(cal.getWeekday(1971, 1, 1) == 4);
+			System.out.println(cal.getWeekday(1972, 1, 1) == 5);
+			System.out.println(cal.getWeekday(1973, 1, 1) == 0);
+			System.out.println(cal.getWeekday(1974, 1, 1) == 1);
+
+		}
 	
 }
--- src/javaCalender/Prompt.java
+++ src/javaCalender/Prompt.java
@@ -24,7 +24,7 @@ public class Prompt {
 		
 		int month = -1;
 		int year = -1;
-		int weekday = 0;
+		
 		
 		while(true) {
 			System.out.println("년도를 입력하세요.(exit: -1)");
@@ -37,15 +37,12 @@ public class Prompt {
 			System.out.println(MONTH);
 			month = scanner.nextInt();
 			
-			System.out.println("첫째 날의 요일을 입력하세요.");
-			String str_weekday = scanner.next();
-			weekday = parseDay(str_weekday);
-
+		
 			if(month > 12 || month < 1) {
 				System.out.println("잘못된 입력입니다");
 				continue;
 			}
-			cal.printCalender(year, month, weekday);
+			cal.printCalender(year, month);
 		}
 		
 		System.out.println("Bye~");
